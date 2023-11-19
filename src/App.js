c1f48f90dec9fdf9bd73381d39014afdbd584cab
mahdy alsayed <mahdyalsayed6@gmail.com>
2023-11-19 13:16:21 +0200

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/Navbar";
+import "./App.css";
+import Home from "./component/Home";
+import { Route, Routes } from "react-router-dom";
+import Products from "./component/Products";
+import Product from "./component/Product";
+import Cart from "./component/Cart";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" Component={Home} />
+                <Route exact path="/products" Component={Products} />
+                <Route exact path="/products/:id" Component={Product} />
+                <Route exact path="/cart" Component={Cart} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
